7cf04cbc800ece887a589e428d04a102a5bb16c7
Stuart Shelton <stuart@shelton.me>
2014-04-24 00:03:33 +0100

    Remove accidentally-inserted junk characters, general clean-up following static analysis

--- udp-proxy.c
+++ udp-proxy.c
@@ -43,31 +43,37 @@ int bindsocket( char* ip, int port );
 int main( int argc, char* argv[] );
 
 int bindsocket( char* ip, int port ) {
-	int fd = socket( PF_INET, SOCK_DGRAM, IPPROTO_IP );
+	int fd;
 	struct sockaddr_in addr;
 
 	addr.sin_family = AF_INET;
 	addr.sin_addr.s_addr = inet_addr( ip );
 	addr.sin_port = htons( port );
-	if( bind( fd, (struct sockaddr*)&addr, sizeof( addr ) ) == -1 ) {
+
+	fd = socket( PF_INET, SOCK_DGRAM, IPPROTO_IP );
+	if( -1 == bind( fd, (struct sockaddr*)&addr, sizeof( addr ) ) ) {
 		fprintf( stderr, "Cannot bind address (%s:%d)\n", ip, port );
 		exit( 1 );
 	}
+
 	return fd;
 }
 
 int main( int argc, char* argv[] ) {
+	int i, listen, output;
+	char *inip, *inpt, *srcip, *dstip, *dstpt;
+	struct sockaddr_in src;
+	struct sockaddr_in dst;
+	struct sockaddr_in ret;
+
 	if( 3 != argc && 5 != argc && 6 != argc ) {
-		fprintf( stderr, "Usage: %s <listen-ip> <listen-port> [[source-ip] <destination-ip> <destination-port>]\n", argv[0] );
+		fprintf( stderr, "Usage: %s <listen-ip> <listen-port> [[source-ip] <destination-ip> <destination-port>]\n", argv[ 0 ] );
 		exit( 1 );
 	}
 
-	int i = 1;
-	char* inip = argv[ i++ ];	/* 1 */
-	char* inpt = argv[ i++ ];	/* 2 */
-	char* srcip;
-	char* dstip;
-	char* dstpt;
+	i = 1;
+	inip = argv[ i++ ];		/* 1 */
+	inpt = argv[ i++ ];		/* 2 */
 	if( 6 == argc )
 		srcip = argv[ i++ ];	/* 3 */
 	if( 3 != argc ) {
@@ -75,34 +81,30 @@ int main( int argc, char* argv[] ) {
 		dstpt = argv[ i++ ];	/* 4 or 5 */
 	}
 	
-	int listen = bindsocket( inip, atoi( inpt ) );
-	int source = 0;
-	int output = 0;
-	if( 6 != argc ) {
-		output = listen;
+	listen = bindsocket( inip, atoi( inpt ) );
+	if( 6 == argc ) {
+		output = bindsocket( srcip, atoi( inpt ) );
 	} else {
-		source = bindsocket( srcip, atoi( inpt ) );
-		output = source;
+		output = listen;
 	}
 
-	struct sockaddr_in dst;
-	dst.sin_family = AF_INET;
-	dst.sin_addr.s_addr = inet_addr( dstip );
-	dst.sin_port = htons( atoi( dstpt ) );
-
-	struct sockaddr_in src;
-	struct sockaddr_in ret;
+	if( 3 != argc ) {
+		dst.sin_family = AF_INET;
+		dst.sin_addr.s_addr = inet_addr( dstip );
+		dst.sin_port = htons( atoi( dstpt ) );
+	}
 	ret.sin_addr.s_addr = 0;
+
 	while( 1 ) {
 		char buffer[65535];
-		int size = sizeof( src );
+		unsigned int size = sizeof( src );
 		int length = recvfrom( listen, buffer, sizeof( buffer ), 0, (struct sockaddr*)&src, &size );
 		if( length <= 0 )
 			continue;
 
-		if( argc == 3 ) {
+		if( 3 == argc ) {
 			/* echo, without tracking return packets */
-			sendto( listen, buffer, length, 0, (sth, 0, (struct sockaddr*)&src, size );
+			sendto( listen, buffer, length, 0, (struct sockaddr*)&src, size );
 		} else if( ( src.sin_addr.s_addr == dst.sin_addr.s_addr ) && ( src.sin_port == dst.sin_port ) ) {
 			/* If we receive a return packet back from our destination ... */
 			if( ret.sin_addr.s_addr )
@@ -111,7 +113,7 @@ int main( int argc, char* argv[] ) {
 				sendto( output, buffer, length, 0, (struct sockaddr*)&ret, sizeof( ret ) );
 		} else {
 			sendto( output, buffer, length, 0, (struct sockaddr*)&dst, sizeof( dst ) );
-			/* Remeber original sender to direct return packets toward */
+			/* Remeber original sender to direct return packets towards */
 			ret = src;
 		}
 	}
